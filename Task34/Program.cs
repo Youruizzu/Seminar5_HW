68fe6f89597f3681fe15dc6a88a2e22cfd1c1139
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-26 23:09:45 +0300

    Added Task34 solution

---
+++ Task34/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+void FillArray(int[] collection){
+    for(int i = 0; i < collection.Length; i++){
+        collection[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] col){
+    for(int i = 0; i < col.Length; i++){
+        Console.Write($"{col[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int EvenNums(int[] array){
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        if (array[i]%2==0) count++;
+    }
+    return count;
+}
+
+int[] array = new int[20];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine($"Количество чётных чисел в массиве = {EvenNums(array)}.");
