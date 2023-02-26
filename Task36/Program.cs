9a855737d3619225819e565f2aa4b93935d6b63f
Sergei Youruizzu <icykle.wolf@gmail.com>
2023-02-26 23:15:54 +0300

    Added Task36 solution

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+void FillArray(int[] collection){
+    for(int i = 0; i < collection.Length; i++){
+        collection[i] = new Random().Next(1, 100);
+    }
+}
+
+void PrintArray(int[] col){
+    for(int i = 0; i < col.Length; i++){
+        Console.Write($"{col[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int UnevenSum(int[] array){
+    int sum = 0;
+    for(int i = 1; i < array.Length; i++){
+        if (i%2==1) sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine($"Сумма чисел стоящих на нечётных позициях в массиве = {UnevenSum(array)}.");
